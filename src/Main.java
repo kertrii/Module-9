2f11693ec06d692321e8fcc353b577e3bb076770
Kateryna <kateryna299@gmail.com>
2024-06-23 11:18:01 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -71,21 +71,28 @@ public class Main {
         System.out.println();
 
 
-        MyHashMap<String, String> map = new MyHashMap<>();
+//        MyHashMap<String, String> map = new MyHashMap<>();
 
-        map.put("key1", "value1");
-        map.put("key2", "value2");
-        map.put("key3", "value3");
+//        map.put("key1", "value1");
+//        map.put("key2", "value2");
+//        map.put("key3", "value3");
 
-        System.out.println("Size of HashMap: " + map.size());
+//        System.out.println("Size of HashMap: " + map.size());
 
-        System.out.println("Value for 'key1': " + map.get("key1"));
-        System.out.println("Value for 'key2': " + map.get("key2"));
-        System.out.println("Value for 'key3': " + map.get("key3"));
+//        System.out.println("Value for 'key1': " + map.get("key1"));
+//        System.out.println("Value for 'key2': " + map.get("key2"));
+//        System.out.println("Value for 'key3': " + map.get("key3"));
 
-        map.remove("key2");
-        System.out.println("Value for 'key2' after removal: " + map.get("key2"));
-        System.out.println("Size after removal: " + map.size());
+//        map.remove("key2");
+//        System.out.println("Value for 'key2' after removal: " + map.get("key2"));
+//        System.out.println("Size after removal: " + map.size());
+
+        MyHashMap<String, Integer> mhm = new MyHashMap<>();
+        mhm.put(null, 666);
+//        mhm.put(null, 7777);
+        System.out.println(mhm.get(null)); //  виводиться 7777
+        System.out.println(mhm.size()); // виводиться 1
+        mhm.remove(null);
 
         list.clear();
         System.out.println("Size after clearing: " + list.size());
